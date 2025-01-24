2bfbce14915e1e97942568b55b28aa372fcbed3d
charitha-nvg <charitha.waravita@novigi.com.au>
2025-01-24 06:14:25 +0000

    imported my new comp to this main comp

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MyNewComponentComponent} from './my-new-component/my-new-component.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MyNewComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
